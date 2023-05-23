ba0affebd4001264b3edc967719c990e746f11c7
juansubas <juansubas@gmail.com>
2023-05-23 16:44:29 -0500

    Actualizando Version Funcional Con Razor Pages

--- AppNetRazor/AppNetRazor/Pages/Cursos/Index.cshtml.cs
+++ AppNetRazor/AppNetRazor/Pages/Cursos/Index.cshtml.cs
@@ -1,12 +1,23 @@
+using AppNetRazor.Datos;
+using AppNetRazor.Modelos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppNetRazor.Pages.Cursos
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicacionDbContext _contexto;
+        public IEnumerable<Curso> Cursos { get; set; }
+
+        public IndexModel(ApplicacionDbContext contexto)
         {
+            _contexto = contexto;
+        }
+        public async Task OnGet()
+        {
+            Cursos = await _contexto.Curso.ToListAsync();
         }
     }
 }
